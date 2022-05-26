9eddf197d74ea33b10d6d9966077575ae9b5f8a0
Supermalonz <kavekhunsri@gmail.com>
2022-05-26 13:53:42 +0700

    added 2 endpoints

--- app/api/v1/keywords_api.rb
+++ app/api/v1/keywords_api.rb
@@ -2,12 +2,26 @@
 
 module V1
   class KeywordsAPI < Grape::API
-
     namespace :keywords do
+      desc 'Get /api/v1/keywords/adswords' do
+        summary 'Get word that contains Adwords the most'
+      end
+      get :adswords do
+        pivot = {}
+        keywords = Keyword.all
+        keywords.each do |keyword|
+          key_id = keyword.title.to_sym
+          pivot[key_id] = SearchResult.where(is_ads: true).where(keyword_id: keyword.id).count
+        end
+        key_result = pivot.key(pivot.values.max)
+        value_result = pivot[pivot.key(pivot.values.max)]
+        render_json(:ok, "#{key_result} : #{value_result}")
+      end
+
       desc 'Get /api/v1/keywords/all' do
         summary 'Get all result'
       end
-      get do
+      get :all do
         result = SearchResult.all
         render_json(:ok, result.as_json)
       end
@@ -26,6 +40,20 @@ module V1
       get :non_ads do
         result = SearchResult.where(is_ads: false)
         render_json(:ok, result.as_json)
+      end
+
+      desc 'Get /api/v1/keywords/contains' do
+        summary 'Get URLs contains the word'
+      end
+      params do
+        optional :query, type: String, desc: 'Query string'
+      end
+      get :contains do
+        count = 0
+        SearchResult.all.each do |row|
+          count += 1 if row.url.include? params[:query]
+        end
+        render_json(:ok, "#{params[:query]} : #{count}")
       end
 
       desc 'Post /api/v1/keywords/csv' do
